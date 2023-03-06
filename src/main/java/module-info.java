20c406c15360ed738774337b1512a202111618c9
unknown <bogdanredspice@gmail.com>
2023-03-06 14:33:51 +0200

    in

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.pt2023_3022_laszlo_bogdan_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.pt2023_3022_laszlo_bogdan_1 to javafx.fxml;
+    exports com.example.pt2023_3022_laszlo_bogdan_1;
+}
